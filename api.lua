789f9dea923b16b251c0bdd06428d019f881b562
Tom van Dijck <tvandijck@blizzard.com>
2016-05-05 14:25:33 -0700

    More methods to check things.
    We now support:
    - check_include
    - check_type_size
    - check_struct_has_member
    - check_symbol_exists

--- api.lua
+++ api.lua
@@ -6,6 +6,7 @@ local p = premake
 local autoconf = p.modules.autoconf
 autoconf.cache = {}
 
+
 ---
 -- register autoconfigure api.
 ---
@@ -16,69 +17,52 @@ p.api.register {
 }
 
 ---
--- try compiling a piece of c/c++
----
-function try_compile(cfg, cpp)
-	local ts = autoconf.toolset(cfg)
-	if ts then
-		return ts.try_compile(cfg, cpp)
-	else
-		p.warnOnce('autoconf', 'no toolset found, autoconf always failing.')
-	end
-end
-
----
--- check for the a particular include file.
+-- Check for a particular include file.
+--
+-- @cfg      : Current config.
+-- @variable : The variable to store the result, such as 'HAVE_STDINT_H'.
+-- @filename : The header file to check for.
 ---
 function check_include(cfg, variable, filename)
-	local res = autoconf.cache[variable]
-	if res == nil then
-		local cpp = p.capture(function ()
-			p.outln('#include <' .. filename .. '>')
-			p.outln('int main(void) { return 0; }')
-		end)
+	local res = autoconf.cache_compile(cfg, variable, function ()
+		p.outln('#include <' .. filename .. '>')
+		p.outln('int main(void) { return 0; }')
+	end)
 
-		res = try_compile(cfg, cpp)
-		if res ~= nil then
-			autoconf.cache[variable] = true
-		else
-			autoconf.cache[variable] = false
-		end
+	if res.value then
+		autoconf.set_value(cfg, variable, 1)
 	end
-
-	if res then
-		p.outln("#define " .. variable)
-	else
-		p.outln("// #undef " .. variable)
-	end
-
-	return res
 end
 
 
 ---
--- check for size of a particular type.
+-- Check for size of a particular type.
+--
+-- @cfg      : Current config.
+-- @variable : The variable to use, such as 'SIZEOF_SIZE_T', this method will also add "'HAVE_' .. variable".
+-- @type     : The type to check.
+-- @headers  : An optional array of header files to include.
 ---
-function check_type_size(cfg, variable, type)
+function check_type_size(cfg, variable, type, headers)
 	check_include(cfg, 'HAVE_SYS_TYPES_H', 'sys/types.h')
 	check_include(cfg, 'HAVE_STDINT_H', 'stdint.h')
 	check_include(cfg, 'HAVE_STDDEF_H', 'stddef.h')
 
-	local res = autoconf.cache[variable .. cfg.platform]
-	if not res then
-		local cpp = p.capture(function ()
-			if autoconf.cache['HAVE_SYS_TYPES_H'] then
+	local res = autoconf.cache_compile(cfg, variable .. cfg.platform, 
+		function ()
+			if cfg.autoconf['HAVE_SYS_TYPES_H'] then
 				p.outln('#include <sys/types.h>')
 			end
 
-			if autoconf.cache['HAVE_STDINT_H'] then
+			if cfg.autoconf['HAVE_STDINT_H'] then
 				p.outln('#include <stdint.h>')
 			end
 
-			if autoconf.cache['HAVE_STDDEF_H'] then
+			if cfg.autoconf['HAVE_STDDEF_H'] then
 				p.outln('#include <stddef.h>')
 			end
 
+			autoconf.include_headers(headers)
 			p.outln("")
 			p.outln("#define SIZE (sizeof(" .. type .. "))")
 			p.outln("char info_size[] =  {'I', 'N', 'F', 'O', ':', 's','i','z','e','[',")
@@ -88,41 +72,114 @@ function check_type_size(cfg, variable, type)
 			p.outln("  ('0' + ((SIZE / 10)%10)),")
 			p.outln("  ('0' +  (SIZE     %10)),")
 			p.outln("  ']', '\\0'};")
-
+			p.outln("")
 			p.outln("int main(int argc, char *argv[]) {")
 			p.outln("  int require = 0;")
 			p.outln("  require += info_size[argc];")
 			p.outln("  (void)argv;")
 			p.outln("  return require;")
 			p.outln("}")
-		end)
-
-		res = try_compile(cfg, cpp)
-		if res then
-			local content = io.readfile(res)
+		end,
+		function (e)
+			-- if the compile step succeeded, we should have a binary with 'INFO:size[*****]'
+			-- somewhere in there.
+			local content = io.readfile(e.binary)
 			if content then
 				local size = string.find(content, 'INFO:size')
 				if size then
-					res = tonumber(string.sub(content, size+10, size+14))
-				else
-					res = false
+					e.size = tonumber(string.sub(content, size+10, size+14))
 				end
-			else
-				res = false
 			end
 		end
+	)
 
-		-- cache result.
-		autoconf.cache[variable .. cfg.platform] = res
+	if res.size then
+		autoconf.set_value(cfg, 'HAVE_' .. variable, 1)
+		autoconf.set_value(cfg, variable, res.size)
 	end
+end
 
-	if res then
-		p.outln('#define HAVE_' .. variable)
-		p.outln('#define ' .. variable .. ' ' .. res)
-		return true
+
+---
+-- Check if the given struct or class has the specified member variable
+--
+-- @cfg      : current config.
+-- @variable : variable to store the result.
+-- @type     : the name of the struct or class you are interested in
+-- @member   : the member which existence you want to check
+-- @headers  : an optional array of header files to include.
+---
+function check_struct_has_member(cfg, variable, type, member, headers)
+	local res = autoconf.cache_compile(cfg, variable, function ()
+		autoconf.include_headers(headers)
+		p.outln('int main(void) {')
+		p.outln('  (void)sizeof(((' .. type .. '*)0)->' .. member ..');')
+		p.outln('  return 0;')
+		p.outln('}')
+	end)
+
+	if res.value then
+		autoconf.set_value(cfg, variable, 1)
+	end
+end
+
+
+---
+-- Check if a symbol exists as a function, variable, or macro
+--
+-- @cfg      : current config.
+-- @variable : variable to store the result.
+-- @symbol   : The symbol to check for.
+-- @headers  : an optional array of header files to include.
+---
+function check_symbol_exists(cfg, variable, symbol, headers)
+	local res = autoconf.cache_compile(cfg, variable, function ()
+		autoconf.include_headers(headers)
+		p.outln('int main(int argc, char** argv) {')
+		p.outln('  (void)argv;')
+		p.outln('#ifndef ' .. symbol)
+		p.outln('  return ((int*)(&' .. symbol .. '))[argc];')
+		p.outln('#else')
+		p.outln('  (void)argc;')
+		p.outln('  return 0;')
+		p.outln('#endif')
+		p.outln('}')
+	end)
+
+	if res.value then
+		autoconf.set_value(cfg, variable, 1)
+	end
+end
+
+
+---
+-- try compiling a piece of c/c++
+---
+function autoconf.try_compile(cfg, cpp)
+	local ts = autoconf.toolset(cfg)
+	if ts then
+		return ts.try_compile(cfg, cpp)
 	else
-		p.outln("// #undef " .. variable)
+		p.warnOnce('autoconf', 'no toolset found, autoconf always failing.')
 	end
+end
+
+
+function autoconf.cache_compile(cfg, entry, func, post)
+	if not autoconf.cache[entry] then
+		local cpp = p.capture(func)
+		local res = autoconf.try_compile(cfg, cpp)
+		if res then
+			local e = { binary = res, value = true }
+			if post then
+				post(e)
+			end
+			autoconf.cache[entry] = e
+		else
+			autoconf.cache[entry] = { }
+		end
+	end
+	return autoconf.cache[entry]
 end
 
 
@@ -146,6 +203,48 @@ function autoconf.toolset(cfg)
 	return ts
 end
 
+
+---
+-- store the value of the variable in the configuration
+---
+function autoconf.set_value(cfg, variable, value)
+	cfg.autoconf = cfg.autoconf or {}
+	cfg.autoconf[variable] = value
+end
+
+
+---
+-- write the cfg.autoconf table to the file
+---
+function autoconf.writefile(cfg, filename)
+	if cfg.autoconf then
+		print(filename)
+
+		local file = io.open(filename, "w+")
+		for variable, value in pairs(cfg.autoconf) do
+			file:write('#define ' .. variable .. ' ' .. tostring(value) .. (_eol or '\n'))
+		end
+		file:close()
+	end
+end
+
+
+---
+-- Utility method to add a table of headers.
+---
+function autoconf.include_headers(headers)
+	if headers ~= nil then
+		if type(headers) ~= "table" then
+			for _, v in ipairs(headers) do
+				p.outln('#include <' .. v .. '>')
+			end
+		else
+			p.outln('#include <' .. headers .. '>')
+		end
+	end
+end
+
+
 ---
 -- attach ourselfs to the running action.
 ---
@@ -162,12 +261,11 @@ p.override(p.action, 'call', function (base, name)
 			if cfg.autoconfigure then
 				verbosef('Running auto config steps for "%s/%s".', prj.name, cfg.name)
 				for file, func in pairs(cfg.autoconfigure) do
-					local filename = path.join(cfg.objdir, file)
-					local result = p.capture(function()
-						func(cfg)
-					end)
+					func(cfg)
+
 					os.mkdir(cfg.objdir)
-					os.writefile_ifnotequal(result, filename)
+					local filename = path.join(cfg.objdir, file)
+					autoconf.writefile(cfg, filename)
 				end
 			end
 		end

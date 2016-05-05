27b3cf50fc7fc2a64828509a2b20f80ba8c9e3ba
Tom van Dijck <tvandijck@blizzard.com>
2016-05-04 17:11:55 -0700

    Attempt at autoconf on linux & mac.

--- clang.lua
+++ clang.lua
@@ -2,3 +2,22 @@
 -- Autoconfiguration.
 -- Copyright (c) 2016 Blizzard Entertainment
 ---
+local p = premake
+local clang = p.tools.clang
+
+function clang.try_compile(cfg, text)
+	-- write the text to a temporary file.
+	local cppFile = path.join(cfg.objdir, "temp.cpp")
+	if not io.writefile(cppFile, text) then
+		return nil
+	end
+
+	local outFile = path.join(cfg.objdir, "temp.out")
+
+	-- compile that text file.
+	if os.execute('clang "' .. cppFile .. '" -o "' .. outFile ..'"') then
+		return outFile
+	else
+		return nil
+	end
+end
